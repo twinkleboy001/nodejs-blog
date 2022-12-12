0acee8a1cd271518bb04dd66e0eb4779d4adfa80
zhouhao <zhouhao01@troy.cn>
2022-12-12 15:31:04 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const serverHandle = (req, res) => {
+  // 设置返回格式JSON
+  res.setHeader("Content-type", "application/json");
+
+  const resData = {
+    name: "双越",
+    site: "imooc",
+    env: process.env.NODE_ENV,
+  };
+
+  res.end(JSON.stringify(resData));
+};
+
+module.exports = serverHandle;
